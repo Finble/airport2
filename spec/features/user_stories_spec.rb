70b0bec1eb4b218a5a071cf7e22b3718c332ae71
Finble <elainefinn@hotmail.com>
2016-06-08 17:05:11 +0100

    User Story 2 completed:
      As an air traffic controller
      So I can get passengers on the way to their destination
      I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -9,4 +9,15 @@ describe 'User stories' do
 		plane = Plane.new
 		expect{airport.land(plane)}.not_to raise_error
 	end
+
+	# As an air traffic controller 
+	# So I can get passengers on the way to their destination 
+	# I want to instruct a plane to take off from an airport 
+
+	it 'so planes take off at airports, instruct a plane to take off' do
+		airport = Airport.new
+		plane = Plane.new
+		expect{airport.take_off(plane)}.not_to raise_error
+	end
+
 end
